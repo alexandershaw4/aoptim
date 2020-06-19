56b257f3650c4e03f74c45da8c952c5d4c44d9b7
Alex <alexandershaw4@gmail.com>
2020-06-19 17:38:16 +0100

    +++

---
+++ Examples/Example_FunctionWithInteractingStates.m
@@ -0,0 +1,25 @@
+clear global ; clear; close all;
+
+% Example function with interacting states...
+f  = @(x) ( sum(toeplitz(x)) )' ;
+
+x  = 2:12; % actual optimsed values to find
+y  = f(x); % data given correct parameters
+
+rng default;
+
+x0 = randn(size(x));     % random start positions
+V  = ones(size(x0))/512; % variances/step sizes
+
+op = AO('options');
+op.step_method = 3;
+op.fun = f;
+op.x0  = x0(:);
+op.y   = y(:);
+op.V   = V(:);
+
+op.step_method = 3;
+op.maxit       = 256;
+
+[X,F,CV] = AO(op);
+

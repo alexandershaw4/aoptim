21f4169891f89d8f973be0ed6e19ca054ac4cad7
Alexander Shaw <alexandershaw@Alexanders-MacBook-Pro.local>
2021-08-19 15:08:54 +0100

    updated nn

--- neuralnet/visualisenn.m
+++ neuralnet/visualisenn.m
@@ -1,4 +1,6 @@
-function visualisenn(m)
+function visualisenn(m,v)
+
+m.modelspace = spm_unvec(v,m.modelspace);
 
 if isa(m,'AONN') && any(strcmp(properties(m),'modelspace'))
     m = m.modelspace;
@@ -7,7 +9,7 @@ end
 figure('Name','AO','Color',[.3 .3 .3],'InvertHardcopy','off',...
     'position',[706         380        1226         486]);
 
-s(1) = subplot(141); imagesc(m{1});
+s(1) = subplot(151); imagesc(m{1});
 ax = gca;
 ax.XGrid = 'off';
 ax.YGrid = 'on';
@@ -19,7 +21,7 @@ s(1).Color  = [.3 .3 .3];
 ylabel('Inputs','fontsize',18);xlabel('Hidden Layer: Neurons','fontsize',18);
 title('MAPPING','color','w','fontsize',18);
 
-s(2) = subplot(142); imagesc(diag(1./(1+exp(-m{2}))));
+s(2) = subplot(152); imagesc(diag(1./(1+exp(-m{2}))));
 ax = gca;
 ax.XGrid = 'off';
 ax.YGrid = 'on';
@@ -32,7 +34,20 @@ axis square;
 xlabel('Diagonals','fontsize',18);
 title('Neuron Activation Functions','color','w','fontsize',18);
 
-s(3) = subplot(143); imagesc(m{3});
+s(3) = subplot(153); imagesc(m{3});
+ax = gca;
+ax.XGrid = 'off';
+ax.YGrid = 'on';
+ax.XTick = [];
+ax.YTick = [];
+s(3).YColor = [1 1 1];
+s(3).XColor = [1 1 1];
+s(3).Color  = [.3 .3 .3];
+ylabel('Hidden Layer Neurons'' Self-Gain','fontsize',18);xlabel('OUTPUTS','fontsize',18);
+title('MAPPING','color','w','fontsize',18);
+
+
+s(4) = subplot(154); imagesc(m{4});
 ax = gca;
 ax.XGrid = 'off';
 ax.YGrid = 'on';
@@ -44,7 +59,7 @@ s(3).Color  = [.3 .3 .3];
 ylabel('Hidden Layer: Neurons','fontsize',18);xlabel('OUTPUTS','fontsize',18);
 title('MAPPING','color','w','fontsize',18);
 
-s(4) = subplot(144); imagesc(diag(1./(1+exp(-m{4}))));
+s(5) = subplot(155); imagesc(diag(1./(1+exp(-m{5}))));
 ax = gca;
 ax.XGrid = 'off';
 ax.YGrid = 'on';
